a310ec32751e74e44bfb7c79bb6dfba9095ba0e9
bekzhaan93 <105780906+bekzhaan93@users.noreply.github.com>
2025-08-18 02:45:57 +0500

    Add files via upload

---
+++ .github/workflows/service-worker.js
@@ -0,0 +1,29 @@
+
+const CACHE_NAME = 'bekcalc-cache-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png', './boqcalc.css'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then(cached => {
+      if (cached) return cached;
+      return fetch(req).then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(req, resClone));
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
